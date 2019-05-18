dd7c3c4c930d17b929ecc4a19225332d9e54b4d1
hugobayoud <49056475+hugobayoud@users.noreply.github.com>
2019-05-18 11:54:48 +0200

    Add files via upload

---
+++ resolutionAvecGeneration/include/GUROBI.h
@@ -0,0 +1,12 @@
+#ifndef __GUROBI_H__
+#define __GUROBI_H__
+
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+
+/*[PROTOTYPE]*/
+void LancementGurobi(char *Fichier1, char *Fichier2);
+void LancementGurobi2();
+
+#endif
